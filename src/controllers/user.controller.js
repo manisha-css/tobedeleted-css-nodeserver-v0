77856b72c691b5301ffce80ebabe8852cf17ecd4
Manisha <manisha_msathe@yahoo.com>
2019-07-13 15:55:04 -0700

    separation of validator

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,5 @@
-const Joi = require('@hapi/joi');
 const InfoResponse = require('../dto/inforesponse');
 const logger = require('../shared/logger');
-const userSchemaInsertValidation = require('../validators/user.validator.schema');
 
 exports.createUserAndSendEmail = async (req, res) => {
   const reqUserObj = {};
@@ -10,16 +8,7 @@ exports.createUserAndSendEmail = async (req, res) => {
   reqUserObj.password = req.body.password;
   reqUserObj.cnfpassword = req.body.cnfpassword;
 
-  // Return result.
-  Joi.validate(reqUserObj, userSchemaInsertValidation, (error, value) => {
-    if (error) {
-      const infoResponseError = new InfoResponse(res.translate(error.details[0].message));
-      res.status(400).json(infoResponseError);
-    } else {
-      const infoResponse = new InfoResponse(res.translate('user.register.success'));
-      logger.info('Inside User API');
-      logger.info(`value: ${value}`);
-      res.status(200).json(infoResponse);
-    }
-  });
+  const infoResponse = new InfoResponse(res.translate('user.register.success'));
+  logger.info('Inside User API');
+  res.status(200).json(infoResponse);
 };
