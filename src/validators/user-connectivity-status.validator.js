4c307b6da21a992cc2e479cfc56da0b16228115a
Manisha <manisha_msathe@yahoo.com>
2019-11-14 08:42:01 +0530

    small format issue

--- src/validators/user-connectivity-status.validator.js
+++ src/validators/user-connectivity-status.validator.js
@@ -3,8 +3,10 @@ exports.UserConnectivityStatusInputValidator = async payload => {
   if (
     payload.socketId !== undefined &&
     payload.socketId !== null &&
-    payload.userId !== undefined && payload.userId > 0 &&
-    payload.status !== undefined && payload.status !== null
+    payload.userId !== undefined &&
+    payload.userId > 0 &&
+    payload.status !== undefined &&
+    payload.status !== null
   )
     return true;
   return false;
